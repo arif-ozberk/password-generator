7997f5351ee88c81ec46ad95c3e71d5f4f31508d
arif-ozberk <ozberkazmak@gmail.com>
2023-08-12 13:00:14 +0300

    fix settings selection bug

--- script.js
+++ script.js
@@ -20,22 +20,49 @@ const symbols = document.querySelector(".symbols")
 
 let charList = []
 
+const numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbolList = [",", ".", "-", "/", "*", "?", "$", "+", "&", "!", "'", "^", "=", ":", "_"]
+const lowerCharList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const upperCharList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
+// Number list functions
 function addNumbers(){
-    charList.push("0","1","2","3","4","5","6","7","8","9")
+    charList.push(numberList)
 }
 
+function removeNumbers() {
+    charList.splice(charList.indexOf(numberList), 1)
+}
+
+
+// Symbol list functions
 function addSymbols(){
-    charList.push("," , "." , "-" , "/" , "*" , "?" , "$" , "+" , "&" , "!" , "'" , "^" , "=" , ":" , "_")
+    charList.push(symbolList)
+}
+
+function removeSymbols() {
+    charList.splice(charList.indexOf(symbolList), 1)
 }
 
 
+// Uppercase character list functions
 function upperCaseFunc(){
-    charList.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
-
+    charList.push(upperCharList)
 }
 
+function removeUpperCase() {
+    charList.splice(charList.indexOf(upperCharList), 1)
+}
+
+
+// Lowercase character list functions
 function lowerCaseFunc(){
-    charList.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    charList.push(lowerCharList)
+}
+
+function removeLowerCase() {
+    charList.splice(charList.indexOf(lowerCharList), 1)
 }
 
 
@@ -50,70 +77,97 @@ function deleteDuplicates(){
 }
 
 
-
 function config(){
     uL.forEach((input, index) =>{
         input.addEventListener("input", ()=>{
-            if(input.className.includes("lowercase") && input.checked){
-                lowerCaseFunc()
-                console.log("lowercase")
+            if(input.className.includes("lowercase")){
+                if(input.checked) {
+                    console.log("lowercase bastim")
+                    lowerCaseFunc()
+                }
+                else {
+                    console.log("lower çektim")
+                    removeLowerCase()
+                }
             }
             
-            if(input.className.includes("uppercase") && input.checked){
-                upperCaseFunc()
-                console.log("uppercase")
+            if(input.className.includes("uppercase")){
+                if(input.checked) {
+                    upperCaseFunc()
+                }
+                else {
+                    removeUpperCase()
+                }
             }
             
-            if(input.className.includes("numbers") && input.checked){
-                addNumbers()
-                console.log("numbers")
+            if(input.className.includes("numbers")){
+                if(input.checked) {
+                    addNumbers()
+                }
+                else {
+                    removeNumbers()
+                }
+            }
+
+            if (input.className.includes("symbols")) {
+                if(input.checked) {
+                    addSymbols()
+                }
+                else {
+                    removeSymbols()
+                }
             }
             
             if(input.className.includes("duplicate") && input.checked){
                 deleteDuplicates()
                 console.log("duplicate")
             }
-            
-            if(input.className.includes("symbols") && input.checked){
-                addSymbols()
-                console.log("symbols")
-            }
-                
-    
-        } )
+        })
     })
     
 }
 
+
 function passwordLengthFunc(){
     passwordLength = lenghtSlider.value
     passwordLengthText.innerHTML = passwordLength
-    // console.log(passwordLength)
 }
+
 
 function generatePassword(){
-    config()
+    if(charList.length === 0) {
+        window.alert("Please choose at least one option!")
+        return
+    }
+
     passwordLengthFunc()
-    // console.log(charList)
+
     createdPassword = ""
     for(let k=0; k<passwordLength; k++){
-        let randomIndex = charList[(Math.floor(Math.random()*charList.length))]
-        // console.log(randomIndex)
-        createdPassword += randomIndex
-
+        let charPool = charList[Math.floor(Math.random() * charList.length)]
+        let randomCharacter = charPool[Math.floor(Math.random() * charPool.length)]
+        createdPassword += randomCharacter
     }
+
     generatedPassword.innerHTML = createdPassword
-    console.log(createdPassword)
 }
+
 
 function copyToClipboard(){
+    if(createdPassword.length === 0) {
+        window.alert("Nothing to be copied!")
+        return
+    }
+
     navigator.clipboard.writeText(createdPassword);
 }
+
 
 window.addEventListener("load", ()=>{
     config()
     passwordLengthFunc()
 })
+
 
 copyButton.addEventListener("click", copyToClipboard)
 generateButton.addEventListener("click", generatePassword)
